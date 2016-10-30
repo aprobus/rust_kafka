7da47e06fa186f4688c552f77baa1e809f848b97
Aaron Probus <aprobus15@gmail.com>
2016-10-29 17:40:39 -0700

    Reads/writes in fixed size

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,26 @@ use std::mem;
 use std::io::prelude::*;
 use crc::{crc32, Hasher32};
 
+const CRC_OFFSET: usize = 0;     // 0-3
+const LEN_OFFSET: usize = 4;     // 4-7
+const TYPE_OFFSET: usize = 8;    // 8
+const PAYLOAD_OFFSET: usize = 9; // 9 - ??
+
+const NUM_HEADER_BYTES: usize = 9; // crc(4) + length(4) + type(1)
+
 fn main() {
-    {
-        let mut f = File::create("foo.txt").unwrap();
-        let payload: Vec<u8> = vec!(8, 1, 3);
-        write_payload(&mut f, &payload);
+    let mut buffer = Vec::with_capacity(256);
+    for _ in 0..buffer.capacity() {
+        buffer.push(0);
     }
 
     {
-        let mut buffer = Vec::with_capacity(256);
-        for _ in 0..buffer.capacity() {
-            buffer.push(0);
-        }
+        let mut f = File::create("foo.txt").unwrap();
+        let payload: Vec<u8> = vec!(8, 1, 3);
+        write_payload(&mut f, &mut buffer, &payload,);
+    }
 
+    {
         let mut f = File::open("foo.txt").unwrap();
         let payload = read_payload(&mut f, &mut buffer);
         println!("Resulting payload: {:?}", payload);
@@ -26,61 +33,129 @@ fn main() {
     println!("Hello world");
 }
 
-fn write_payload(file: &mut File, payload: &Vec<u8>) {
-    let len_bytes: [u8; 8] = unsafe { std::mem::transmute(payload.len()) };
-    println!("Writing length ({}): {:?}", payload.len(), len_bytes);
+#[derive(Copy, Clone)]
+enum ChunkType {
+    Full = 0,
+    Start = 1,
+    Middle = 2,
+    End = 3
+}
 
-    let record_crc = calculate_crc(payload.len(), payload);
-    let crc_bytes: [u8; 4] = unsafe { std::mem::transmute(record_crc) };
-    println!("Digest ({:?}): {}", crc_bytes, record_crc);
+impl ChunkType {
+    fn from_byte(x: u8) -> ChunkType {
+        match x {
+            x if x == ChunkType::Full as u8 => ChunkType::Full,
+            x if x == ChunkType::Start as u8 => ChunkType::Start,
+            x if x == ChunkType::Middle as u8 => ChunkType::Middle,
+            x if x == ChunkType::End as u8 => ChunkType::End,
+            _ => panic!("Unknown chunk type"),
+        }
+    }
+}
 
-    file.write_all(&crc_bytes).expect("Failed to write length");
-    file.write_all(&len_bytes).expect("Failed to write length");
-    file.write_all(&payload).expect("Failed to write");
+fn write_payload(file: &mut File, buffer: &mut Vec<u8>, payload: &Vec<u8>) {
+    let num_payload_bytes_per_chunk = buffer.capacity() - NUM_HEADER_BYTES;
+
+    // 1. Exact
+    // payload_size = 10
+    // chunk_size = 10
+    //
+    // (10 + 10 - 1) / 10
+    // 19 / 10
+    // 1 chunk
+    //
+    // 2. Partial
+    // payload_size = 12
+    // chunk_size = 10
+    //
+    // (12 + 10 - 1) / 10
+    // 21 / 10
+    // 2 chunks
+    let num_chunks = (payload.len() + num_payload_bytes_per_chunk - 1) / num_payload_bytes_per_chunk;
+
+    let mut chunks_iter = payload.chunks(num_payload_bytes_per_chunk).enumerate();
+    while let Some((i, next_chunk)) = chunks_iter.next() {
+        write_chunk(file, buffer, next_chunk, i, num_chunks);
+    }
 
     file.flush().expect("Failed to flush");
     file.sync_all().expect("Failed to sync");
 }
 
-fn read_payload(file: &mut File, buffer: &mut Vec<u8>) -> Result<Vec<u8>, &'static str> {
-    let read_size = file.read(buffer).unwrap();
+fn write_chunk(file: &mut File, buffer: &mut Vec<u8>, payload: &[u8], chunk_index: usize, num_chunks: usize) {
+    write_u32(buffer, 0, CRC_OFFSET);
+    write_u32(buffer, payload.len() as u32, LEN_OFFSET);
 
-    if read_size < 12 {
-        panic!("Read less than crc(4) + length(8) bytes!");
-    }
-
-    let crc_end = mem::size_of::<u32>();
-    let len_end = crc_end + mem::size_of::<usize>();
-
-    //TODO: Little Endian vs Big Endian safety
-
-    let expected_crc: u32 = read_u32(&buffer, 0, crc_end);
-    let payload_len = read_usize(&buffer, crc_end, len_end);
-
-    let mut payload = Vec::with_capacity(payload_len);
-    for i in len_end..read_size {
-        payload.push(buffer[i]);
-    }
-    while payload.len() < payload_len {
-        let read_size = file.read(buffer).unwrap();
-        for i in 0..read_size {
-            payload.push(buffer[i]);
-        }
-    }
-
-    let actual_crc = calculate_crc(payload_len, &payload);
-
-    if actual_crc == expected_crc {
-        Result::Ok(payload)
+    if chunk_index == 0 && num_chunks == 1 {
+        buffer[TYPE_OFFSET] = ChunkType::Full as u8;
+    } else if chunk_index == 0 {
+        buffer[TYPE_OFFSET] = ChunkType::Start as u8;
+    } else if chunk_index + 1 == num_chunks {
+        buffer[TYPE_OFFSET] = ChunkType::End as u8;
     } else {
-        println!("Mismatched crc: {} vs {}", actual_crc, expected_crc);
-        Result::Err("Mismatched crc")
+        buffer[TYPE_OFFSET] = ChunkType::Middle as u8;
     }
+
+    let mut payload_iter = payload.iter().enumerate();
+    while let Some((i, x)) = payload_iter.next() {
+        buffer[PAYLOAD_OFFSET + i] = *x;
+    }
+
+    for i in (PAYLOAD_OFFSET + payload.len())..(buffer.capacity()) {
+        buffer[i] = 0;
+    }
+
+    let record_crc = calculate_crc(buffer);
+    write_u32(buffer, record_crc, CRC_OFFSET);
+    println!("Digest: {}", record_crc);
+
+    file.write_all(&buffer).expect("Failed to write");
 }
 
-fn read_u32(buffer: &Vec<u8>, start: usize, end: usize) -> u32 {
+fn read_payload(file: &mut File, buffer: &mut Vec<u8>) -> Vec<u8> {
+    let mut payload = Vec::new();
+
+    loop {
+        let read_result = read_chunk(&mut payload, file, buffer).unwrap();
+
+        match read_result {
+            ChunkType::Full | ChunkType::End => break,
+            ChunkType::Start | ChunkType::Middle => continue,
+        };
+    }
+
+    payload
+}
+
+fn read_chunk(payload: &mut Vec<u8>, file: &mut File, buffer: &mut Vec<u8>) -> Result<ChunkType, &'static str> {
+    if let Result::Err(_) = file.read_exact(buffer) {
+        return Result::Err("Unable to read from file")
+    }
+
+    let expected_crc: u32 = read_u32(&buffer, CRC_OFFSET);
+    write_u32(buffer, 0, CRC_OFFSET);
+    let actual_crc = calculate_crc(buffer);
+
+    if expected_crc != actual_crc {
+        return Result::Err("CRC did not much expected value")
+    }
+
+    let chunk_len = read_u32(&buffer, LEN_OFFSET) as usize;
+    let chunk_type = ChunkType::from_byte(buffer[TYPE_OFFSET]);
+
+    payload.reserve(chunk_len);
+    for i in 0..chunk_len {
+        payload.push(buffer[PAYLOAD_OFFSET + i]);
+    }
+
+    Result::Ok(chunk_type)
+}
+
+fn read_u32(buffer: &Vec<u8>, start: usize) -> u32 {
+    let size = mem::size_of::<u32>();
+
     let mut result: u32 = 0;
-    for i in start..end {
+    for i in start..(start + size) {
         let next_byte: u32 = buffer[i] as u32;
         result = (result >> 8) | (next_byte << 24);
     }
@@ -88,21 +163,19 @@ fn read_u32(buffer: &Vec<u8>, start: usize, end: usize) -> u32 {
     result
 }
 
-fn read_usize(buffer: &Vec<u8>, start: usize, end: usize) -> usize {
-    let mut result: usize = 0;
-    for i in start..end {
-        let next_byte: usize = (buffer[i] as usize) << 56;
-        result = (result >> 8) | next_byte;
+fn write_u32(buffer: &mut Vec<u8>, x: u32, index: usize) {
+    let size = mem::size_of::<u32>();
+
+    let mut x_remain = x;
+    for i in 0..size {
+        let byte = x_remain as u8;
+        buffer[index + i] = byte;
+        x_remain = x_remain >> 8;
     }
-    result
 }
 
-fn calculate_crc(length: usize, payload: &Vec<u8>) -> u32 {
+fn calculate_crc(payload: &Vec<u8>) -> u32 {
     let mut digest = crc32::Digest::new(crc32::IEEE);
-
-    let len_bytes: [u8; 8] = unsafe { std::mem::transmute(length) };
-    digest.write(&len_bytes);
-
     digest.write(&payload);
     digest.sum32()
 }
